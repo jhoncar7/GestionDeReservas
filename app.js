5f7b63b70e2e26304779061b50d934bb23579d89
00frank <68354976+00frank@users.noreply.github.com>
2021-05-19 21:27:36 -0300

    express functions and typo
    borrado de la importacion del modulo body-parser, para usar las funciones de express (ya que son recomendadas por el mismo paquete)
    typo en middelware => middleware

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const chalk = require('chalk');
-const middelware = require('./routers/middleware')
+const middleware = require('./routers/middleware')
 const apis = require('./routers/apis');
 
 const app = express();
 const path = 8080;
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(middelware);
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(middleware);
 app.use(apis);
 
 app.listen(path,()=>{
