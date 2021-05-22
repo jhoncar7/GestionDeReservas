ddf12ef84d918d7aa1f4dc5050b8c346ae4d8046
JHON JAIRO CARREÑO LOPEZ <95831198@comunidadort.edu.ar>
2021-05-22 20:08:25 -0300

    nuevos arreglos

---
+++ routers/admin.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const dataMetodoGet = require('../data/metodoGet');
+
+router.get('/abm-usuario', (req, res) => {
+    res.render('abmUsuario',{usuario : req.session.usuario})
+})
+
+router.get('/abm-area', (req, res) => {
+    res.render('abmArea',{usuario : req.session.usclearuario})
+})
+
+module.exports = router;
