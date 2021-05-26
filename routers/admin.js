d65c16c83c581e894ad945cb4eb157b99ab32556
Frank Garcia <00frankgarcia@gmail.com>
2021-05-26 19:57:51 -0300

    cambios en funcionalidad del admin e identado

--- routers/admin.js
+++ routers/admin.js
@@ -7,73 +7,69 @@ const dataMetodoPost = require('../data/metodoPost');
 
 router.get('/abm-usuario', async (req, res) => {
     const user = await dataMetodoGet.getUsers();
-    const usuario = req.session.usuario;
-    res.render('abmUsuario',{usuario : req.session.usuario, users : user, error:req.flash('error')})
+    res.render('abmUsuario', { usuario: req.session.usuario, users: user, error: req.flash('error') })
 });
 
-router.get('/abm-area', async(req, res) => {
+router.get('/abm-area', async (req, res) => {
     const areas = await dataMetodoGet.getAreas();
-    res.render('abmArea',{usuario : req.session.usuario, area : areas})
+    res.render('abmArea', { usuario: req.session.usuario, area: areas })
 });
 
-router.get('/registroUsuario', async(req, res) => {
+router.get('/registroUsuario', async (req, res) => {
     const areas = await dataMetodoGet.getAreas();
     const perfiles = await dataMetodoGet.getPerfiles();
-    res.render('registroAbmUsuario',{usuario : req.session.usuario, areas:areas, perfiles:perfiles})
+    res.render('registroAbmUsuario', { usuario: req.session.usuario, areas: areas, perfiles: perfiles })
 });
 
-router.post('/procesar_registro_usuario', async(req, res) => {
-    let email = req.body.email;
-    let password = req.body.contrasena;
-    let perfil = req.body.perfil;
-    let area = req.body.area;
-    if(!email || !password || perfil=='' || area==''){
+router.post('/procesar_registro_usuario', async (req, res) => {
+    let { email, password, perfil, area } = req.body;
+    if (!email || !password || !perfil || !area) {
         req.flash('error', 'Error en los datos ingresados')
         res.redirect('/admin/abm-usuario')
-    }else{
+    } else {
         usuario = await dataMetodoPost.addUsuario(req.body);
         res.redirect('/admin/abm-usuario')
     }
 });
 
 router.get('/crearArea', (req, res) => {
-    res.render('crearArea',{usuario : req.session.usuario})
+    res.render('crearArea', { usuario: req.session.usuario })
 });
 
-router.get('/editar/:id', async(req, res) => {
+router.get('/editar/:id', async (req, res) => {
     const user = await dataMetodoGet.getUser(req.params.id);
     const areas = await dataMetodoGet.getAreas();
     const perfiles = await dataMetodoGet.getPerfiles();
-    res.render('editar',{usuario : req.session.usuario, usuario_id: user, areas:areas, perfiles:perfiles})
+    res.render('editar', { usuario: req.session.usuario, usuario_id: user, areas: areas, perfiles: perfiles })
 });
 
-router.post('/procesar_registro_Area', async(req, res) => {
+router.post('/procesar_registro_Area', async (req, res) => {
     let name = req.body.area;
-    if(!name){
+    if (!name) {
         res.redirect('/admin/abm-area')
-    }else{
+    } else {
         name = req.body;
-        a = await dataMetodoPost.addArea(name);
+        await dataMetodoPost.addArea(name);
         res.redirect('/admin/abm-area')
     }
 });
 
-router.post('/procesar_editar', async(req, res) => {
+router.post('/procesar_editar', async (req, res) => {
     let usuario = req.body;
     let id = req.body._id;
     const update = await dataMetodoPut.updateUser(usuario, id);
     res.redirect('/admin/abm-usuario');
 });
 
-router.get('/editarArea/:id', async(req, res) => {
+router.get('/editarArea/:id', async (req, res) => {
     const area = await dataMetodoGet.getArea(req.params.id);
-    res.render('editarArea',{usuario : req.session.usuario, area_id: area})
+    res.render('editarArea', { usuario: req.session.usuario, area_id: area })
 });
 
-router.post('/procesar_editar_area', async(req, res) => {
+router.post('/procesar_editar_area', async (req, res) => {
     let area = req.body;
     let id = req.body._id;
-    const update = await dataMetodoPut.updateArea(area,id);
+    const update = await dataMetodoPut.updateArea(area, id);
     res.redirect('/admin/abm-area');
 });
 
