37d8be2a2f882705cb3b935433cf91771f8bafc6
Agustina <42482268+agusmachalani@users.noreply.github.com>
2021-06-08 19:14:37 -0300

    Update metodoPost.js

--- data/metodoPost.js
+++ data/metodoPost.js
@@ -4,7 +4,7 @@ const bcryptjs = require("bcryptjs");
 
 async function addUsuario(usuario){
     const clientmongo = await connection.getConnection();
-    const contrasena = bcryptjs.hashSync(usuario.password, 8);
+    const contrasena = bcryptjs.hashSync(usuario.contrasena, 8);
     const result = await clientmongo.db('ReservasPuesto')
         .collection('users')
         .insertOne(usuario);
